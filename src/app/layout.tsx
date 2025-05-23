d2c5103351106e1cf83c36f1a6f21c2c6db6d2ae
thinhpham311-dev <phamthinh30522@gmail.com>
2025-05-23 20:34:05 +0700

    create font Epilogue and weight size for text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Epilogue } from 'next/font/google';
 import "./globals.css";
 import { APP_NAME, APP_DES } from "@/constants/app.constant"
 import ReduxProvider from "@/providers/ReduxProvider"
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+
+export const epilogue = Epilogue({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+  display: 'swap',
+  variable: '--font-epilogue', // <-- thêm dòng này
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: APP_NAME,
@@ -26,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${epilogue.variable}`}
       >
         <ReduxProvider >
           {children}
