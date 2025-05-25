89f62d51c7aaa8b02cc675c19680a8a6ae2b7d7a
thinhpham311-dev <phamthinh30522@gmail.com>
2025-05-25 19:21:30 +0700

    update grid and card

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -10,43 +10,41 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ item }: ProductCardProps) => {
     return (
-        <div className="col-span-1" >
-            <Card className='bg-white p-3 h-full flex flex-col  gap-y-3'>
-                <CardImage src={item.imageUrl} alt={item?.name}
-                    className='aspect-square  relative'
-                    imageClassName="object-contain h-full w-full"
-                />
-                <CardHeader className='p-0 flex-grow'>
-                    <p className="inline-flex flex-wrap gap-2">
-                        {item.isHotDeal && (
-                            <Badge className="bg-badge-gradient text-danger-dark font-semibold inline-flex space-x-2">
-                                <span><FireIcon /></span>
-                                <span>Giá cực sốc</span>
-                            </Badge>
-                        )}
-                    </p>
-                    <CardTitle className='line-clamp-2 font-semibold text-base text-text-primary '>
-                        {item?.name}
-                    </CardTitle>
-                </CardHeader>
+        <Card className='bg-white p-3 h-full flex flex-col  gap-y-3'>
+            <CardImage src={item.imageUrl} alt={item?.name}
+                className='aspect-square  relative'
+                imageClassName="object-contain h-full w-full"
+            />
+            <CardHeader className='p-0 flex-grow'>
+                <p className="inline-flex flex-wrap gap-2">
+                    {item.isHotDeal && (
+                        <Badge className="bg-badge-gradient text-danger-dark font-semibold inline-flex space-x-2">
+                            <span><FireIcon /></span>
+                            <span>Giá cực sốc</span>
+                        </Badge>
+                    )}
+                </p>
+                <CardTitle className='line-clamp-2 font-semibold text-base text-text-primary '>
+                    {item?.name}
+                </CardTitle>
+            </CardHeader>
 
-                <CardContent className='flex flex-col gap-y-1 p-0'>
-                    <p className='text-base text-danger font-semibold'>
-                        <span>{formatCurrency(item?.discountedPrice)}</span>
-                    </p>
-                    <p className="text-sm text-text-secondary font-normal inline-flex space-x-2 items-center">
-                        <span className="line-through">{formatCurrency(item?.originalPrice)}</span>
-                        <span className="text-danger font-medium">-{item?.discountPercentage}%</span>
-                    </p>
-                </CardContent>
+            <CardContent className='flex flex-col gap-y-1 p-0'>
+                <p className='text-base text-danger font-semibold'>
+                    <span>{formatCurrency(item?.discountedPrice)}</span>
+                </p>
+                <p className="text-sm text-text-secondary font-normal inline-flex space-x-2 items-center">
+                    <span className="line-through">{formatCurrency(item?.originalPrice)}</span>
+                    <span className="text-danger font-medium">-{item?.discountPercentage}%</span>
+                </p>
+            </CardContent>
 
-                <CardFooter className="p-0 mt-3">
-                    <Button type="button" variant='secondary' className='w-full'>
-                        <span className='text-primary'>Mua Ngay</span>
-                    </Button>
-                </CardFooter>
-            </Card>
-        </div>
+            <CardFooter className="p-0 mt-3">
+                <Button type="button" variant='secondary' className='w-full'>
+                    <span className='text-primary'>Mua Ngay</span>
+                </Button>
+            </CardFooter>
+        </Card>
     );
 };
 
