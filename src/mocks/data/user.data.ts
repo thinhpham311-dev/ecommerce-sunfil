ce63c3df909d5ee20b6d045b8d6427ee90e09b69
thinhpham311-dev <phamthinh30522@gmail.com>
2025-05-27 12:05:55 +0700

    update dropdown user

---
+++ src/mocks/data/user.data.ts
@@ -0,0 +1,9 @@
+import { IUser } from "@/interfaces/IUser"
+
+export const USER_DATA: IUser[] = [
+    {
+        id: "user-001",
+        name: "Phạm trí Thịnh",
+        email: "thinhpham67ag@gmail.com",
+    }
+]
